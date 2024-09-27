d5533be8cdae9c62556d67c2aceff58209215cf3
loujilin <15167181260@139.com>
2024-09-27 15:49:00 +0800

    cpp-27-3

--- Time.cpp
+++ Time.cpp
@@ -2,8 +2,18 @@
 // Created by 楼吉林 on 2024/9/27.
 //
 #include <iostream>
-
+struct Time1{
+    int hour;
+    int minute;
+    int second;
+};
+class Time{
+    int hour;
+    int minute;
+    int second;
+};
 using namespace std;
+
 int main()
 {
     return 0;
